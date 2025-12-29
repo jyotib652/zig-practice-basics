46f355da08fd1bca86bbad4ca3665fcf84905926
Jyotiprokash Banerjee <jyotib652@gmail.com>
2025-12-29 16:55:09 +0530

    added more comments and explanations.

--- 6_chars_strings.zig
+++ 6_chars_strings.zig
@@ -45,6 +45,8 @@ pub fn main() void {
     print("Concatenated string: {s}\n", .{concat_string}); // Don't forget to use {s} for strings to print strings.
     // String must be an array of unsigned 8-bit integers (u8). Each character in the string is represented by its ASCII value.
     // String are immutable by default.
+    // Sting can be concatenated using the ++ operator.
+    // String can be  repeated using the ** operator.
 
     // Multi line strings
     const multiple_strings =
@@ -60,4 +62,12 @@ pub fn main() void {
     // b_string[2] = 'C'; // This will cause a compile-time error
     b_string = "tiger"; // But we can reassign the whole string of the same length.
     print("b_string: {s}\n", .{b_string});
+
+    // The type of a string literal is a single item pointer to a constant null terminated byte array.
+    // *const [14:0]u8   -> @TypeOf(String Literal)
+    // *const -> (Single item) pointer to a constant
+    // [14:0]u8          -> Array of 14 u8 elements + null terminator:
+    //     u8               -> Each element is an unsigned 8-bit integer (byte)
+    //     [14:0]           -> Array has 14 elements (0 to 13) + null terminator at index 14
+
 }

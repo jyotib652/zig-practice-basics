c601f49ac45b83e1ae954ce678ca92ec16121638
Jyotiprokash Banerjee <jyotib652@gmail.com>
2026-01-07 12:19:31 +0530

    added more switch scenarios

--- 17_switch.zig
+++ 17_switch.zig
@@ -57,4 +57,69 @@ pub fn main() void {
         true => print("a_boolean is true \n", .{}),
         false => print("a_boolean is fale \n", .{}),
     }
+
+    // Different function return types. print & dummyFunction
+    // switch (thirteen) {
+    //     0 => print("value is 0 \n", .{}),
+    //     1 => dummyFunction(), // the error says retun types do not match. print returns void but dummyFunction returns bool
+    //     2 => print("value is 2 \n", .{}),
+    //     3, 4 => print("value is either 3 or 4 \n", .{}),
+    //     5...10 => print("value is between 5 and 10 (inclusive) \n", .{}),
+    //     10 + 1 => print("value is 11 \n", .{}), // For this operation, values should comptime known
+    //     twelve => print("value is 12 \n", .{}),
+    //     // thirteen => print("value is 13 \n", .{}),
+    //     else => print("value is something else \n", .{}),
+    // }
+
+    // Solution to different function return types within switch statement
+    switch (thirteen) {
+        0 => print("value is 0 \n", .{}),
+        1 => {
+            _ = dummyFunction(); // wrap dummyFunction in a block and since here we don't need the value discard it.
+        },
+        2 => print("value is 2 \n", .{}),
+        3, 4 => print("value is either 3 or 4 \n", .{}),
+        5...10 => print("value is between 5 and 10 (inclusive) \n", .{}),
+        10 + 1 => print("value is 11 \n", .{}), // For this operation, values should comptime known
+        twelve => print("value is 12 \n", .{}),
+        // thirteen => print("value is 13 \n", .{}),
+        else => print("value is something else \n", .{}),
+    }
+
+    // Besides primitive types like numbers & booleans, switch can be done on enums & tagged unions
+    const data_type_enum: DataTypes = DataTypes.integer;
+
+    switch (data_type_enum) {
+        DataTypes.integer => print("enum is an integer \n", .{}),
+        DataTypes.float => print("enum is float \n", .{}),
+        else => print("enum is something else"),
+    }
+
+    // We can determine the active field of the tagged union and run some logic based on that with switch
+    const tagged_union: TaggedUnion = TaggedUnion{ .float = 3.14 };
+    switch (tagged_union) {
+        TaggedUnion.integer => print("Integer field is active \n", .{}),
+        TaggedUnion.float => |val| print("Float field is active: {} \n", .{val}),
+        TaggedUnion.boolean => print("Boolean field is active \n", .{}),
+    }
+
+    // If we make the tagged_union a variable then we can modify the value within switch statement using a pointer
+    var another_tagged_union: TaggedUnion = TaggedUnion{ .float = 3.14 };
+    switch (another_tagged_union) {
+        TaggedUnion.integer => print("Integer field is active \n", .{}),
+        TaggedUnion.float => |*val| {
+            val.* = 3.141;
+            print("Float field is active: {} \n", .{val.*});
+        },
+        TaggedUnion.boolean => print("Boolean field is active \n", .{}),
+    }
+
+    print("another_tagged_union after the switch: {} \n", .{another_tagged_union});
+}
+
+const DataTypes = enum { integer, float, boolean };
+const TaggedUnion = union(DataTypes) { integer: u8, float: f32, boolean: bool };
+
+fn dummyFunction() bool {
+    return true;
 }
